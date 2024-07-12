d931a7d2068802bd1c2a1db703f8dbb79ad97e3b
zepetzi <zepetzi@gmail.com>
2024-07-12 03:44:25 -0700

    basic img upload func prototype complete

--- src/components/UploadForm.jsx
+++ src/components/UploadForm.jsx
@@ -1,28 +1,48 @@
 import { useRef, useState } from 'react';
-import { auth } from '../firebase';
+import { auth, storageRef, storage } from '../firebase';
+import { v4 as uuid} from "uuid" 
+import { uploadBytes, ref } from "firebase/storage";
+
+
 
 export default function UploadForm() {
-    const [image, setImage] = useState(null);
-    const imageURLRef = useRef(null);
 
-    const uploadImage = (evt) => {
-        const addedImage = evt.target.files[0];
-        const imageURL = URL.createObjectURL(addedImage)
-        imageURLRef.current = imageURL;
-    }
+    const [selectedFile, setSelectedFile] = useState(null);
 
-    const handleButtonClick = () => {
-        const imageURL = imageURLRef.current;
-        if (imageURL) {
-            setImage(imageURL);
-        }
+    const handleSelectFile = (evt) => {
+        const addedFile = evt.target.files[0] 
+        setSelectedFile(addedFile)
     };
+
+    const handleButtonClick = async () => {
+        
+        if (selectedFile) {
+            try {
+                //make file URL for image preview
+                const selectedFileURL = URL.createObjectURL(selectedFile);
+
+                try{
+
+                    const uploadedImgPostRef = await ref(storage, `post_images/${uuid()}.jpg`);
+                    uploadBytes(uploadedImgPostRef, selectedFile);
+
+                } catch (error) {
+                    window.alert(error);        
+                }
+
+            } catch (error) {
+                window.alert(error);      
+            }
+        } else {
+            window.alert("No file selected");
+        }
+
+    }
 
     return (
         <>
-        <input type="file" onChange={uploadImage}/>
+        <input type="file" onChange={handleSelectFile}/>
         <button onClick={handleButtonClick}>Upload</button>
-        {/* <img src={image}></img> */}
         </>
     )
 };
