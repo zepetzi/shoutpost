1e8548a8eb3142f8e46096d700a0526bc8088fe9
zepetzi <zepetzi@gmail.com>
2024-08-10 03:00:18 -0700

    small edit

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -5,16 +5,25 @@ import { doc, onSnapshot } from "firebase/firestore"
 // import './Canvas.css' 
 import useImage from 'use-image';
 
+//https://storage.googleapis.com/{bucket}/{name}
 
 const TEST_CANVAS = '761ee601-074d-41a3-a417-f864abe78fc1_canvas';
 
 export default function Canvas(){
 
-    //setup listener for RT updates on image posts
-    const unsub = onSnapshot(doc(fsdb, "canvases", `${TEST_CANVAS}`), (doc) => {
-        console.log("Current data: ", doc.data().owner);
-    });
+const [posts, setPosts] = useState([]);
 
+    try {
+        //setup listener for RT updates on image posts
+        const unsub = onSnapshot(doc(fsdb, "canvases", `${TEST_CANVAS}`), (docSnapshot) => {
+            if (docSnapshot.exists()) {
+                console.log("Current data: ", docSnapshot.data().current_posts);
+            }
+        });
+
+    } catch(error) {
+
+    }
     // const testURL = URL.createObjectURL('../../public/vite.svg')
     const [testImage] = useImage('../../public/vite.svg'); 
 
